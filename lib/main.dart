fc556faa48d996d454677e40e45a9a40cce128a3
Muhammad Burhan Mujahid <muhammadburhan.mujahid@techswivel.com>
2025-06-26 22:21:33 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fuellogic/config/theme.dart';
@@ -5,9 +6,11 @@ import 'package:fuellogic/core/routes/app_router.dart';
 import 'package:fuellogic/modules/auth/screens/register_screen.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
